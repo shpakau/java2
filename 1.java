58048a9ba18e65d676fc0943e1f97b481e4668c5
shpakau <51748821+shpakau@users.noreply.github.com>
2023-02-17 09:46:01 +0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,10 @@
+package StringTesting;
+import java.util.Scanner;
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = sc.nextInt();
+        System.out.println("ваше число "+ a);
+    }
+}
